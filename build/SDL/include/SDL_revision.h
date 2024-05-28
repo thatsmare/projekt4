a6594d1427fd74301b46596504f96fab09b7e69f
thatsmare <mpenkowska0@gmail.com>
2024-05-28 19:36:22 +0200

    base

--- build/SDL/include/SDL_revision.h
+++ build/SDL/include/SDL_revision.h
@@ -1,4 +1,4 @@
-#define SDL_REVISION "https://github.com/thatsmare/projekt4.git@"
+#define SDL_REVISION "https://github.com/thatsmare/projekt4.git@1f41a353a5a4da1b3b8d47c0c6ae6b14c0351d2a"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
