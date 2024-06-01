4e2f8e25b597ad98b8801440ce3f8a61da9131e2
thatsmare <mpenkowska0@gmail.com>
2024-06-01 21:30:04 +0200

    setup

--- build/SDL/include/SDL_revision.h
+++ build/SDL/include/SDL_revision.h
@@ -1,4 +1,4 @@
-#define SDL_REVISION "https://github.com/thatsmare/projekt4.git@1f41a353a5a4da1b3b8d47c0c6ae6b14c0351d2a"
+#define SDL_REVISION "https://github.com/thatsmare/projekt4.git@a6594d1427fd74301b46596504f96fab09b7e69f"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
