93ff52d80d67e134e620a7373e780de353c6b619
thatsmare <mpenkowska0@gmail.com>
2024-06-09 21:54:38 +0200

    crazy flying but spins

--- PlanarQuadrotor/planar_quadrotor_visualizer.cpp
+++ PlanarQuadrotor/planar_quadrotor_visualizer.cpp
@@ -23,30 +23,33 @@ void PlanarQuadrotorVisualizer::render(std::shared_ptr<SDL_Renderer> &gRenderer)
 
     SDL_SetRenderDrawColor(gRenderer.get(), 0x00, 0x00, 0xFF, 0xFF);
 
-    float half_lenght = 30;
-    float half_height = 10;
+    float half_length = 30;
+    float half_height = 5;
     int half_width_screen = SCREEN_WIDTH/2;
     int half_hight_screen = SCREEN_HEIGHT/2;
 
+    float angle_velocity = state[3];
+    q_theta *= angle_velocity * 0.5 * 0.0002; //trying to make it less spinny
+
     // x_output = x_original*cos(theta) - y_original*sin(theta)
     // y_output = x_original*sin(theta) + y_original*cos(theta)
-    float x_1 = q_x + half_width_screen - half_lenght;
-    float x_2 = q_x + half_width_screen + half_lenght;
-    float x_3 = q_x + half_width_screen - half_lenght;
-    float x_4 = q_x + half_width_screen + half_lenght;
+    float x_1 = q_x + half_width_screen + half_length * cos(q_theta) - half_height * sin(q_theta);
+    float x_2 = q_x + half_width_screen - half_length * cos(q_theta) - half_height * sin(q_theta);
+    float x_3 = q_x + half_width_screen - half_length * cos(q_theta) + half_height * sin(q_theta);
+    float x_4 = q_x + half_width_screen + half_length * cos(q_theta) + half_height * sin(q_theta);
 
-    float y_1 = q_y + half_hight_screen - half_height;
-    float y_2 = q_y + half_hight_screen - half_height;
-    float y_3 = q_y + half_hight_screen + half_height;
-    float y_4 = q_y + half_hight_screen + half_height;
+    float y_1 = q_y + half_hight_screen + half_length * sin(q_theta) + half_height * cos(q_theta);
+    float y_2 = q_y + half_hight_screen - half_length * sin(q_theta) + half_height * cos(q_theta);
+    float y_3 = q_y + half_hight_screen - half_length * sin(q_theta) - half_height * cos(q_theta);
+    float y_4 = q_y + half_hight_screen + half_length * sin(q_theta) - half_height * cos(q_theta);
 
 
     SDL_RenderDrawLine(gRenderer.get(), x_1, y_1, x_2, y_2);
-    SDL_RenderDrawLine(gRenderer.get(), x_1, y_1, x_3, y_3);
-    SDL_RenderDrawLine(gRenderer.get(), x_2, y_2, x_4, y_4);
+    SDL_RenderDrawLine(gRenderer.get(), x_1, y_1, x_4, y_4);
+    SDL_RenderDrawLine(gRenderer.get(), x_2, y_2, x_3, y_3);
     SDL_RenderDrawLine(gRenderer.get(), x_3, y_3, x_4, y_4);
 
-
+     filledCircleColor(gRenderer.get(), q_x + SCREEN_WIDTH/2, q_y + SCREEN_HEIGHT/2, 10, 0xFF00FF00); 
 
 }
 
--- build/SDL/include/SDL_revision.h
+++ build/SDL/include/SDL_revision.h
@@ -1,4 +1,4 @@
-#define SDL_REVISION "https://github.com/thatsmare/projekt4.git@0846cf23f03ee3fd99b354d364b54d85ad0686bb"
+#define SDL_REVISION "https://github.com/thatsmare/projekt4.git@2db7236e98cabf49afbd2d6e5b43d473b353f1f1"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
