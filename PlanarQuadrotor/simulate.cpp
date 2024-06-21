1830c944a057d73f8c1f890f8f6a22ff6d258a69
xn3tx <165705340+xn3tx@users.noreply.github.com>
2024-06-21 10:07:35 +0200

    audio v2

--- PlanarQuadrotor/simulate.cpp
+++ PlanarQuadrotor/simulate.cpp
@@ -37,7 +37,8 @@ void control(PlanarQuadrotor& quadrotor, const Eigen::MatrixXf& K) {
     quadrotor.SetInput(input - K * quadrotor.GetControlState());
 }
 
-void generate_sound(Uint8* stream, int len)
+//---------------------------------------AUDIO----------------------------------------------------------------
+void generate_sound(Uint8* stream, int len)                    
 {
     const int amplitude = 1;  //amplituda dźwięku
     const double sampling_frequency = 44100.0;
@@ -56,6 +57,7 @@ void generate_sound(Uint8* stream, int len)
             }
         }
 }
+//---------------------------------------------------------------------------------------------
 
 int main(int argc, char* args[])
 {
@@ -98,31 +100,31 @@ int main(int argc, char* args[])
     std::vector<float> theta_history;
     std::vector<float> time_history;
 
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
+    {
+        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    //audio specification config
+
+    SDL_AudioSpec spec;
+    spec.freq = 44100.0;           //czestotliwosc probkowania
+    spec.format = AUDIO_U8;
+    spec.channels = 2;              //stereo - jak leci w lewo to lewy głosnik glosniej itp
+    spec.samples = 4096;
+    spec.callback = NULL;
+
+    SDL_AudioSpec obtained_spec;
+    SDL_AudioDeviceID audio_device = SDL_OpenAudioDevice(NULL, 0, &spec, NULL, 0);
+    if (audio_device == 0) {
+        std::cout << "SDL Audio Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
     if (init(gWindow, gRenderer, SCREEN_WIDTH, SCREEN_HEIGHT) >= 0)
     {
-        if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
-        {
-            std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
-            return -1;
-        }
-
-        //audio specification config
-
-        SDL_AudioSpec spec;
-        spec.freq = 44100.0;           //czestotliwosc probkowania
-        spec.format = AUDIO_U8;
-        spec.channels = 2;              //stereo - jak leci w lewo to lewy głosnik glosniej itp
-        spec.samples = 4096;
-        spec.callback = NULL;
-
-        SDL_AudioSpec obtained_spec;
-        SDL_AudioDeviceID audio_device = SDL_OpenAudioDevice(NULL, 0, &spec, NULL, 0);
-        if (audio_device == 0) {
-            std::cout << "SDL Audio Error: " << SDL_GetError() << std::endl;
-            SDL_Quit();
-            return -1;
-        }
-
         SDL_Event e;
         bool quit = false;
         float delay;
@@ -205,7 +207,7 @@ int main(int argc, char* args[])
             quadrotor.Update(dt);
         }
     }
-    //SDL_CloseAudioDevice(audio_device);
+    SDL_CloseAudioDevice(audio_device);
     SDL_Quit();
 
     return 0;
